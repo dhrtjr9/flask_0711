bb1bb8a5f601d3b0790cdee87649963e8ac76301
Lee Ok Seok <dhrtjr9@gmail.com>
2023-07-12 10:47:33 +0900

    test.html 추가

--- app/__init__.py
+++ app/__init__.py
@@ -3,31 +3,18 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-db = SQLAlchemy() #db는 sqlalchemy로 통제 / ORM
+db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object(config) #config.py 속성
+    # ORM에 대한 설정
+    app.config.from_object(config)
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
 
-    from .views import basic_views, okseok_views
+    from .views import basic_views
     app.register_blueprint(basic_views.fisa)
-    app.register_blueprint(okseok_views.okseok)
-    print(__name__)
-
-    @app.route('/about_me')
-    def about_me():
-        return f'저는 {__name__} 입니다.'
-
-    @app.route('/hello')
-    def hello():
-        return f'안녕하세요'
-
-    @app.route('/bye')
-    def bye():
-        return f'잘가세요'
     
     return app
--- app/views/basic_views.py
+++ app/views/basic_views.py
@@ -1,24 +1,23 @@
-from flask import Blueprint
-from app.models import Question, Answer
 from flask import Blueprint, render_template
-
-                #우리가 부를 이름, flask 프레임궈크가 찾을 이름, 라우팅주소
+from app.models import Question, Answer
+# 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
 fisa = Blueprint('basic', __name__, url_prefix='/')
-
-@fisa.route('/about_me')
-def about_me():
-    return f'저는 {__name__} 입니다' 
-
-@fisa.route('/hello')
-def hello():
-    return f'안녕하세요' 
-
-@fisa.route('/bye')
-def bye():
-    return f'안녕히가세요' 
 
 @fisa.route('/detail/<int:question_id>/')
 def detail(question_id):
-    question = Question.query.get(question_id)
+    # question = Question.query.get(question_id)
+    question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question)
-    
+
+@fisa.route('/list')
+def post_list():
+    question_list = Question.query.all()
+    return render_template('question/question_list.html', question_list=question_list) 
+
+# /loop 라는 uri로 이동하는 화면을 만듭니다. 
+# test.html 파일로 가게 됩니다. test = [ 1, 2, 3, 4, 5] 라는 리스트를 같이 return 합니다. 
+
+@fisa.route('/loop')
+def loop():
+    test = [1, 2, 3, 4, 5]
+    return render_template('test.html', list=test)
